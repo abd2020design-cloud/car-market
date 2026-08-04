1a51065a5b6058a657f8b0f022bffa17f8f25f71
abd2020design <abd2020design@gmail.com>
2026-08-04 19:51:22 +0300

    fix: authorize remote image patterns configurations for supabase bucket and unsplash placeholders

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,31 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  typescript: {
-    // 🌟 إجبار السيرفر على تخطي أخطاء التايب سكريبت أثناء الـ Build
-    ignoreBuildErrors: true,
+  images: {
+    // 🌟 فتح الصلاحية الأمنية المطلقة لظهور كافة الصور القادمة من سيرفرك السحابي Pro ومن المواقع العالمية
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pgfuijzgkigrriibahef.supabase.co', // رابط سيرفرك في سوبابيز Pro لظهور السيارات المرفوعة
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '://unsplash.com', // لظهور الصور الافتراضية وصور الخلفيات الفاخرة
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'flagcdn.com', // لظهور صور أعلام الدول الملونة الفاخرة
+        port: '',
+        pathname: '/**',
+      }
+    ],
   },
-  
 };
 
 export default nextConfig;
+
 
